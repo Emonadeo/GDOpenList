ecaa1cdd6acda64683c1b21e009ce53abaf294e0
Emanuel Pilz <emonadeo@gmail.com>
2022-06-06 03:16:50 +0200

    Trying out openapi-typescript

--- server/src/list/model.ts
+++ server/src/list/model.ts
@@ -1,16 +1,24 @@
 import { Level } from '@prisma/client';
 import { prisma } from '../prisma';
+import { paths } from '../../generated/openapi';
+
+type GetListReply = paths['/list']['get']['responses']['200']['content']['application/json'];
 
 /**
  * Extracts the list of levels from the latest log record
  * @returns List of levels
  */
-export async function getCurrent(): Promise<Level[]> {
+export async function getCurrent(): Promise<GetListReply> {
 	const res = await prisma.listLog.findFirst({
 		include: {
 			list: {
 				include: {
-					level: true,
+					level: {
+						include: {
+							user: true,
+							verifier: true,
+						},
+					},
 				},
 				orderBy: {
 					index: 'asc',
--- server/src/list/route.ts
+++ server/src/list/route.ts
@@ -1,11 +1,14 @@
 import { FastifyPluginAsync, RouteHandlerMethod } from 'fastify';
 import { Level } from '@prisma/client';
 import { getCurrent } from './model';
+import { paths } from '../../generated/openapi';
+
+type GetListReply = paths['/list']['get']['responses']['200']['content']['application/json'];
 
 const plugin: FastifyPluginAsync = async function (fastify) {
 	// Get List
 	fastify.get<{
-		Reply: Level[];
+		Reply: GetListReply;
 	}>('/list', async function (_, res) {
 		res.send(await getCurrent());
 	});
